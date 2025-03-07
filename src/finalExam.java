234e3fbe202495d37ab60b82e156e32ce877a7f7
gkovbasyuk <georgy.kovbasyuk@raftds.com>
2025-03-07 09:45:36 +0300

    обновил контрольное задание

--- src/finalExam.java
+++ src/finalExam.java
@@ -2,8 +2,8 @@ import static java.lang.Math.abs;
 
 public class finalExam {
     public static void main(String[] args) {
-        SimpleAccount deb = new SimpleAccount();
-        CreditAccount cred = new CreditAccount(500);
+        Account deb = new SimpleAccount();
+        Account cred = new CreditAccount(500);
         deb.add(400); //пополнение счета
         deb.add(-100); //ошибка пополнения счет
         deb.pay(100); //оплата
@@ -14,11 +14,30 @@ public class finalExam {
         System.out.println(deb.getBalance()); // 400 - 100 + 125 - 50 = 375
         System.out.println(cred.getBalance()); // -100 + 100 -125 + 50 = -75
     }
-    public static class Account {
+
+    public abstract static class Account {
         long accountSum;
+
+        boolean add(long amount) {
+            return true;
+        }
+
+        boolean pay(long amount) {
+            return true;
+        }
+
+        boolean transfer(Account account, long amount) {
+            return true;
+        }
+
+        long getBalance() {
+            return accountSum;
+        }
     }
 
     public static class SimpleAccount extends Account {
+
+        @Override
         boolean add(long amount) {
             if (amount <= 0) {
                 return false;
@@ -28,6 +47,7 @@ public class finalExam {
             }
         }
 
+        @Override
         boolean pay(long amount) {
             if (amount <= 0) {
                 return false;
@@ -40,6 +60,7 @@ public class finalExam {
             }
         }
 
+        @Override
         boolean transfer(Account account, long amount) {
             if (amount > accountSum) {
                 return false;
@@ -49,14 +70,10 @@ public class finalExam {
                 return true;
             }
         }
-
-        long getBalance() {
-            return accountSum;
-        }
-
     }
 
     public static class CreditAccount extends Account {
+
         long accountLimit;
 
         public CreditAccount(long lim) {
@@ -66,6 +83,7 @@ public class finalExam {
             else this.accountLimit = lim;
         }
 
+        @Override
         boolean add(long amount) {
             if (amount <= 0) {
                 return false;
@@ -76,6 +94,7 @@ public class finalExam {
             else return false;
         }
 
+        @Override
         boolean pay(long amount) {
             if (amount <= 0) {
                 return false;
@@ -88,6 +107,7 @@ public class finalExam {
             }
         }
 
+        @Override
         boolean transfer(Account account, long amount) {
             if (amount <= 0) {
                 return false;
@@ -99,10 +119,6 @@ public class finalExam {
                 this.accountSum -= amount;
                 return true;
             }
-        }
-
-        long getBalance() {
-            return accountSum;
         }
     }
 }
